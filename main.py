76dbcc278bc6257c11b8d2507e1e6fc96f7ed6dd
Saron <Saron251004@gmail.com>
2026-03-27 10:24:46 -0400

    adding new branch

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def func1():
     lst = []
     print('Enter numbers one at a time. Enter 0 to stop.')
-    x = int(input('num:'))
-    while x != 0:
-        lst.append(x)
-        x = int(input('num:'))
+    y = int(input('num:'))
+    while y != 0:
+        lst.append(y)
+        y = int(input('num:'))
     return lst
 
 def func2(data):
