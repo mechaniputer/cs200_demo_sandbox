b358fdddca2310f87bc6a22a6eec64f126df5903
Caleb Raughley <calebraughley@gmail.com>
2026-04-03 10:49:01 -0400

    Fix linting errors

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ def func1():
     while myNum != 0:
         lst.append(myNum)
         myNum = int(input('num:'))
-    print('You added ', len(lst),'Numbers')
+    print('You added ', len(lst), ' Numbers')
     return lst
+
 
 def func2(info):
     print('Here is the data:')
     print(info)
-
     response = ''
     while True:
         response = input('Select an option. 1-sum  2-min  3-max  4-quit')
         if response == '1':
-            print('The sum is',sum(info))
+            print('The sum is', sum(info))
         elif response == '2':
             print('Not supported yet')
         elif response == '3':
@@ -24,6 +24,7 @@ def func2(info):
         elif response == '4':
             print('Goodbye')
             return
+
 
 def main():
     print("Hello, World!")
@@ -51,6 +52,7 @@ def main():
     print('Hello World Again')
     data = func1()
     func2(data)
+
 
 if __name__ == "__main__":
     main()
